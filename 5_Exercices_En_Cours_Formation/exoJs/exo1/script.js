6ed8fe6ba29339204137aec690de3dc0d9a88415
theorcd31 <140169050+theorcd31@users.noreply.github.com>
2023-10-17 17:01:20 +0200

    ptitdems

--- 5_Exercices_En_Cours_Formation/exoJs/exo1/script.js
+++ 5_Exercices_En_Cours_Formation/exoJs/exo1/script.js
@@ -23,18 +23,18 @@
 // let nbr1
 
 // if (nbr1 > 0){
-//     console.log("positif")
+//     console.log("positif");
 // } else{
-//     console.log("negatif")
+//     console.log("negatif");
 // }
 
 // //EXERCICE 3 
 //  let nbr2 
 
 //  if(nbr1+nbr2 > 0){
-//     console.log("positif")
+//     console.log("positif");
 //  } else {
-//     console.log("negatif")
+//     console.log("negatif");
 //  }
 
 // //EXERCICE 4
@@ -42,17 +42,17 @@
 // let nbr2 = -5
 
 // if(nbr1 > 0 && nbr2 > 0){
-//     console.log("Positif")
+//     console.log("Positif");
 // } else if ((nbr1 > 0 && nbr2 < 0) || (nbr1 < 0 && nbr2 > 0)){
-//     console.log("Négatif")
+//     console.log("Négatif");
 // } else if (nbr1 == 0 && nbr2 == 0){
-//     console.log("égale à 0")
+//     console.log("égale à 0");
 // } else {
-//     console.log("Négatif")
+//     console.log("Négatif");
 // }
 
 // //EXERCICE 5
-// let age = prompt("Saisir le nombre 1")
+// let age = prompt("Saisir le nombre 1");
 
 // if(age = 6 && 7){
 //     console.log("Poussin");
@@ -63,7 +63,7 @@
 // } else if (age >= 12){
 //     console.log("Cadet");
 // } else {
-//     console.log("Faut avoir au moins 6 ans désolé faut s'entraîner dans le jardin")
+//     console.log("Faut avoir au moins 6 ans désolé faut s'entraîner dans le jardin");
 // }
 
 // //EXERCICE 5 bis
@@ -88,12 +88,61 @@
     
 // }
 
-//EXERCICE 6
+// //EXERCICE 6
 
-let mot1 = prompt("premier mot")
-let mot2 = prompt("second mot")
-let mot3 = prompt("troisième mot")
+// let mot1 = prompt("premier mot");
+// let mot2 = prompt("second mot");
+// let mot3 = prompt("troisième mot");
 
-if(mot1 < mot2 && mot2 < mot3){
-    console.log(mot1+mot2+mot3)
+// if(mot1 < mot2 && mot2 < mot3){
+//     console.log("c alphabétique");
+// } 
+
+// //EXERCICE 7
+// let tab = []; 
+
+// tab[1] = prompt("Saisissez première valeur");
+// tab[2] = prompt("Saisissez deuxième valeur");
+// tab[3] = prompt("Saisissez troisième valeur");
+
+// if(tab[1]<tab[2] && tab[2]< tab[3]){
+//     console.log("los valores se ordenan en orden ascendente");
+// } else {
+//     console.log("les valeurs ne sont pas triées dans un ordre croissant");
+// }
+
+
+// //EXERCICE 8
+
+// let tab = [];
+
+//  tab[1] = {'Produit': prompt("Nom du produit")};
+//  tab[2] = {'PrixHT':parseFloat(prompt("Prix HT"))};
+//  tab[3] = {'Quantité':parseInt(prompt("Quantité"))};
+
+//  tab.push (Math.round((tab[2].PrixHT*1.20)*tab[3].Quantité,2))
+
+//  console.log(tab);
+
+//Exercice 9
+let users = [];
+for (let i=0 ; i<5 ; i++){
+    let user = {
+        'nom' : prompt("Saisir le nom de l'utilisateur"),
+        'prenom': prompt("Saisir le prénom de l'utilisateur"),
+        'age': parseInt(prompt("Saisir l\'age de l'utilisateur")),
+        'numDeTel': parseInt(prompt("Saisir numero de telephone de l'utilisateur"))
+    };
+    users.push(user);
 }
+
+let minAge = users[0].age
+let indice = 0
+
+for (let i=0 ; i<5 ; i++){
+    if(minAge >= users[i].age){
+        minAge = users[i].age
+        indice = i
+    }
+}
+console.log("l'utilisateur le plus jeune est", users[indice].prenom, users[indice].nom)

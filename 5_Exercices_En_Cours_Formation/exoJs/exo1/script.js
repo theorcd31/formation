0f588e90940f677b4641fe0f141eb1b4bb089209
theorcd31 <140169050+theorcd31@users.noreply.github.com>
2023-10-18 17:03:31 +0200

    ajout exos js

--- 5_Exercices_En_Cours_Formation/exoJs/exo1/script.js
+++ 5_Exercices_En_Cours_Formation/exoJs/exo1/script.js
@@ -124,25 +124,123 @@
 
 //  console.log(tab);
 
-//Exercice 9
-let users = [];
-for (let i=0 ; i<5 ; i++){
-    let user = {
-        'nom' : prompt("Saisir le nom de l'utilisateur"),
-        'prenom': prompt("Saisir le prénom de l'utilisateur"),
-        'age': parseInt(prompt("Saisir l\'age de l'utilisateur")),
-        'numDeTel': parseInt(prompt("Saisir numero de telephone de l'utilisateur"))
-    };
-    users.push(user);
-}
+// //Exercice 9
+// let users = [];
+// for (let i=0 ; i<5 ; i++){
+//     let user = {
+//         'nom' : prompt("Saisir le nom de l'utilisateur"),
+//         'prenom': prompt("Saisir le prénom de l'utilisateur"),
+//         'age': parseInt(prompt("Saisir l\'age de l'utilisateur")),
+//         'numDeTel': parseInt(prompt("Saisir numero de telephone de l'utilisateur"))
+//     };
+//     users.push(user);
+// }
 
-let minAge = users[0].age
-let indice = 0
+// let minAge = users[0].age
+// let indice = 0
+
+// for (let i=0 ; i<5 ; i++){
+//     if(minAge >= users[i].age){
+//         minAge = users[i].age
+//         indice = i
+//     }
+// }
+// console.log("l'utilisateur le plus jeune est", users[indice].prenom, users[indice].nom)
+
+// //EXERCICE 10
+//  let devoir = [];
+//  for (let i=0 ; i<2 ; i++){
+//     let Eleve = {
+//         'nom' : prompt("Saisir le nom de l'utilisateur"),
+//         'prenom': prompt("Saisir le prénom de l'utilisateur"),
+//         'note': parseFloat(prompt("Saisir la note")),
+//     };
+//     devoir.push(Eleve);
+// }
+
+// let noteEleves = 0
+// let moyenne = 0 
+
+// for (let i=0 ; i < devoir.length ; i++){
+//     noteEleves += devoir[i].note
+// }
+// moyenne = noteEleves/devoir.length
+
+// if(moyenne < 5){
+//     console.log("MDRRRRRRRRR LES NULLARDS nn c trop faut ouvrir les cahiers à la maison hein", moyenne);
+// } else if (moyenne > 5 && moyenne <= 10){
+//     console.log("c'est pas ouf quand même hein #pasfier2vous", moyenne);
+// } else if (moyenne > 10 && moyenne <= 15){
+//     console.log("gg la teamax mais on peut faire mieux quand même hein #etBelieveCroireEnNosReves", moyenne );
+// } else if (moyenne > 15 && moyenne <= 20){
+//     console.log("MASTERCLAAAASSSSS nn sah c trop y'a pas à être aussi fort hein", moyenne)
+// }
+
+// //EXERCICE 11
+
+// let nbChoco = parseInt(prompt("Saisir le nombre de chocolatine"))
+// let showPrice = 0
+// let nbChoco10 = 10
+
+// if(nbChoco<10){
+//     showPrice = (nbChoco*1.40)
+// } else if(nbChoco>10 && nbChoco <= 20){
+//     showPrice = ((nbChoco-10)*1.30) + (nbChoco10*1.40)
+// } else if(nbChoco>20){
+//     showPrice = ((nbChoco10 * 1.40) + (nbChoco10 * 1.30) + ((nbChoco-20)*1.20))
+// }
+// console.log("Le montant à régler s'élève à", showPrice.toFixed(2))
+
+// //EXERCICE 12
+// let nbChoisi = parseInt(prompt("Saisir un nombre"))
+// let essai = 0
+// let randomNumber
+// let ok = true 
+// randomNumber = parseInt(prompt("Nombre choisi par Fabien Olicard"))
+// while (essai < 10 && ok){
+    
+//     if(randomNumber == nbChoisi){
+//         console.log("GG mon fabien !!! tié un monstre, ton entraînement a payé")
+//         ok = false
+//     }
+//     if (randomNumber < nbChoisi){
+//         console.log("Non mon fab, c'est plus haut")
+//         randomNumber = parseInt(prompt("Nombre choisi par Fabien Olicard"))
+//     } 
+
+//     if (randomNumber > nbChoisi){
+//         console.log("Non mon fab, c'est plus bas")
+//         randomNumber = parseInt(prompt("Nombre choisi par Fabien Olicard"))
+//     } 
+    
+//     if(essai == 9 ){
+//         console.log("Change de métier")
+//         ok = false
+//     }
+//     essai++
+// }
+
+//EXERCICE 13
+
+let banquePoints = 0
+let joueurPoints = 0
 
 for (let i=0 ; i<5 ; i++){
-    if(minAge >= users[i].age){
-        minAge = users[i].age
-        indice = i
+    let banqueDés = Math.floor(Math.random()*5)+1
+    let joueurDés = Math.floor(Math.random()*5)+1
+    console.log("La banque a obtenu un :",banqueDés);
+    console.log("Le joueur a obtenu un :",joueurDés);
+    if(banqueDés > joueurDés){
+        banquePoints += 1
+    } else if(banqueDés == joueurDés){
+        joueurPoints += 2
+    } else {
+        joueurPoints  += 1
     }
 }
-console.log("l'utilisateur le plus jeune est", users[indice].prenom, users[indice].nom)
+
+if(joueurPoints > banquePoints){
+    console.log("Le gagnant est le joueur avec le score de", joueurPoints);
+} else {
+    console.log("Le gagnant est la banque avec le score de", banquePoints);
+}

be1613bab1eb3cce384207853490d85a667575e3
theorcd31 <140169050+theorcd31@users.noreply.github.com>
2023-12-20 16:59:21 +0100

    ajout exos back

---
+++ 5_Exercices_En_Cours_Formation/BACK-END/MySQL/exoSqlBddPhp.sql
@@ -0,0 +1,12 @@
+create database exoPHP;
+use exoPHP;
+
+create table utilisateur(
+id int primary key auto_increment not null,
+nom_utilisateur VARCHAR(50) NOT NULL,
+prenom_utilisateur VARCHAR(50) NOT NULL,
+email_utilisateur VARCHAR(50) NOT NULL,
+password_utilisateur VARCHAR(100) NOT NULL
+);
+
+DROP TABLE utilisateur

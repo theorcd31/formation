384b283e10b1f076022defe10ec9683e112c5dcf
Theo <theo.richard31520@gmail.com>
2023-12-22 16:45:02 +0100

    ajout mcd

--- 3_Mon_Projet_Fil_Rouge/HTML/script.js
+++ 3_Mon_Projet_Fil_Rouge/HTML/script.js
@@ -1,24 +1,6 @@
-const fav = document.querySelector('i');
 const burgerBtn = document.querySelector('.openBurger');
 const menuBurger = document.querySelector('nav');
 const croixBtn = document.querySelector('.closeBurger');
-
-function addFav(){
-    fav.classList.remove('fa-regular');
-    fav.classList.add('fa-solid');
-}
-function delFav(){
-    fav.classList.remove('fa-solid');
-    fav.classList.add('fa-regular');
-}
-
-fav.addEventListener('click', ()=>{
-    if(fav.classList.contains('fa-solid') == true){
-        delFav();
-    } else{
-        addFav();
-    }
-});
 
 burgerBtn.addEventListener('click', ()=>{
     menuBurger.style.transform = 'translateX(0vw)';
@@ -29,3 +11,30 @@ croixBtn.addEventListener('click', ()=>{
     menuBurger.style.transform = 'translateX(100vw)';
 });
 
+
+
+const fav = document.querySelector('#favoris');
+
+function addFav(){
+    fav.classList.remove('fa-regular');
+    fav.classList.add('fa-solid');
+};
+function delFav(){
+    fav.classList.remove('fa-solid');
+    fav.classList.add('fa-regular');
+};
+
+fav.addEventListener('click', ()=>{
+    if(fav.classList.contains('fa-solid') == true){
+        delFav();
+    } else{
+        addFav();
+    }
+});
+
+
+// const test = document.querySelector('.renseignerInfo');
+
+// test.addEventListener('click', ()=>{
+//     console.log('dems');
+// })
